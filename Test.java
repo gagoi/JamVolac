e0a1241f127baefe06d860deced6d919e444b667
gagoi <gagoi@pc-41.home>
2019-01-26 02:29:40 +0100

    MErge

--- Test.java
+++ Test.java
@@ -9,10 +9,6 @@ import fr.wiyochi.level.Loader;
 public class Test {
 
 	public static void main(String[] args) {
-		Loader loader = new Loader();
-		
-		loader.loadLevel(System.getenv("resourcesPath") + "/levels/test.txt");
-		
 		Game.init("TEST", new Dimension(1280, 720), 2);
 		Game.start();
 		
